2a31b7d5426fdf6d8983cf596a3f61b85c8d7188
Alessio Buccino <alejoe9187@gmail.com>
2024-06-13 14:09:13 +0200

    Move Parameters at the top pf the README

--- pipeline/slurm_submit.sh
+++ pipeline/slurm_submit.sh
@@ -12,10 +12,8 @@ conda activate env_nf
 PIPELINE_PATH="path-to-your-cloned-repo"
 DATA_PATH="path-to-data-folder"
 RESULTS_PATH="path-to-results-folder"
-PIPELINE_PARAMS="" # e.g. "--preprocessing_args --debug --debug-duration 120 -resume"
-
 
 NXF_VER=22.10.8 DATA_PATH=$DATA_PATH RESULTS_PATH=$RESULTS_PATH nextflow \
     -C $PIPELINE_PATH/pipeline/nextflow_slurm.config \
     -log $RESULTS_PATH/nextflow/nextflow.log \
-    run $PIPELINE_PATH/pipeline/main_slurm.nf $PIPELINE_PARAMS
+    run $PIPELINE_PATH/pipeline/main_slurm.nf # additional parameters here
