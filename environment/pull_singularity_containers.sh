46f73c523c481ac0678c2f32de99c502d69ca8a0
mbsabath <bsabath1@gmail.com>
2024-08-07 13:37:41 -0400

    path to dir

--- environment/pull_singularity_containers.sh
+++ environment/pull_singularity_containers.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-if [[ -z $1 ]]; then
-    CONTAINER_PATH="/n/holylfs06/LABS/kempner_shared/Everyone/workflow/kilosort25-spike-sorting/containers"
+if [[ -z $EPHYS_CONTAINER_DIR ]]; then
+    CONTAINER_DIR="/n/holylfs06/LABS/kempner_shared/Everyone/workflow/kilosort25-spike-sorting/containers"
 else
-    CONTAINER_PATH=$1
+    CONTAINER_DIR=$1
 fi
 
 for i in `cat task_container_list.txt`; do
     echo $i
-    singularity pull --dir $CONTAINER_PATH docker://$i
+    singularity pull --dir $CONTAINER_DIR docker://$i
 done
